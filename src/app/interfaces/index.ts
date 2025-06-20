613952ad1b97d4a22203039cfd6129402fdd4779
Nahuel Viera <nviera841@gmail.com>
2025-06-19 21:31:17 -0300

    Primer commit del proyecto estudiantes frontend

---
+++ src/app/interfaces/index.ts
@@ -0,0 +1,7 @@
+export type { ActualizarResponse } from './actualizar-response.interface';
+export type { CrearActualizar } from './crearactualizar.interface';
+export type { CrearResponse } from './crear-response.interface';
+export type { EliminarResponse } from './eliminar-response.interface';
+export type { Estudiante } from './estudiante.interface';
+export type { EstudianteResponse } from './estudiante-response.interface';
+export type { EstudiantesResponse } from './estudiantes-response.interface';
